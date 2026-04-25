1ec5e7846a51762deeaed833785c1c58b1447531
ahmad-666 <ahmadrezagghz666@gmail.com>
2026-04-25 13:51:36 +0330

    feat: add new 3 apis

--- snippets/ReadMore.jsx
+++ snippets/ReadMore.jsx
@@ -1,18 +1,15 @@
 export const ReadMore = ({ link, children, className = "" }) => {
   return (
-    <div className={`${className}`}>
-      <h3>Read More</h3>
-      <Button
-        href={link}
-        target="_blank"
-        variant="outline"
-        size="md"
-        color="primary"
-        className="mt-2"
-      >
-        <Icon icon="code" size={24} className="!mint-bg-emerald-600 mr-2" />
-        {children}
-      </Button>
-    </div>
+    <Button
+      href={link}
+      target="_blank"
+      variant="outline"
+      size="md"
+      color="primary"
+      className={`${className}`}
+    >
+      <Icon icon="code" size={24} className="!mint-bg-emerald-600 mr-2" />
+      {children}
+    </Button>
   );
 };
